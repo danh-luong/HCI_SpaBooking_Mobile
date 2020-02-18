95bd0db7445189498d67a03a745ea8ce50ea4cba
Novixous <anonymousvhb@gmail.com>
2020-02-18 16:00:18 +0700

    Initial Commit

--- RosieBeauty/app/src/main/java/rosie/com/rosiebeauty/HomeFragment.java
+++ RosieBeauty/app/src/main/java/rosie/com/rosiebeauty/HomeFragment.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
@@ -14,12 +15,13 @@ import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
-import android.widget.SearchView;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -42,31 +44,45 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_home, container, false);
-        Toolbar toolbar = rootView.findViewById(R.id.my_toolbar);
-        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
-        ((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+//        Toolbar toolbar = rootView.findViewById(R.id.my_toolbar);
+//        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+//        ((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayShowTitleEnabled(false);
         return rootView;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
-        SearchView searchView = view.findViewById(R.id.searchView);
+        SearchView searchView = view.findViewById(R.id.search_view);
         searchView.setOnQueryTextFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
-               Navigation.findNavController(v).navigate(R.id.action_homeFragment_to_searchFragment);
+                NavHostFragment.findNavController(HomeFragment.this).navigate(R.id.action_homeFragment_to_searchFragment);
             }
         });
-
         BottomNavigationView bottomNavigationView = view.findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()){
+                switch (item.getItemId()) {
+                    case R.id.action_home:
+                        NavHostFragment.findNavController(HomeFragment.this).navigate(R.id.action_homeFragment_self);
+                        break;
+                    case R.id.action_favorites:
+                        NavHostFragment.findNavController(HomeFragment.this).navigate(R.id.action_homeFragment_to_favoriteFragment);
+                        break;
                     case R.id.action_profile:
-                        NavHostFragment.findNavController(HomeFragment.this).navigate(R.id.action_homeFragment_to_searchFragment);
+                        NavHostFragment.findNavController(HomeFragment.this).navigate(R.id.action_homeFragment_to_profileFragment);
+                        break;
+                    case R.id.action_book:
+                        NavHostFragment.findNavController(HomeFragment.this).navigate(R.id.action_homeFragment_to_bookingFragment);
+                        break;
                 }
                 return false;
             }
