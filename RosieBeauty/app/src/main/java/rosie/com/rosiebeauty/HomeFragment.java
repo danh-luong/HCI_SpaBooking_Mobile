1a5c06d2c556edc04c3bde1df988cda24b9a839f
Cao Thu <thuctse63589@fpt.edu.vn>
2020-02-18 20:45:19 +0700

    Slide Show in Home Page. Done!

--- RosieBeauty/app/src/main/java/rosie/com/rosiebeauty/HomeFragment.java
+++ RosieBeauty/app/src/main/java/rosie/com/rosiebeauty/HomeFragment.java
@@ -1,31 +1,14 @@
 package rosie.com.rosiebeauty;
 
 
-import android.app.Activity;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.SearchView;
-import androidx.appcompat.widget.Toolbar;
-import androidx.fragment.app.Fragment;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.fragment.NavHostFragment;
-
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
-import android.widget.Toast;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationView;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
 
 /**
@@ -33,59 +16,23 @@ import com.google.android.material.navigation.NavigationView;
  */
 public class HomeFragment extends Fragment {
 
-
+    ViewPager viewSlideShow;
+    SlideshowAdapter slideshowAdapter;
     public HomeFragment() {
         // Required empty public constructor
     }
 
-
+    @SuppressLint("WrongViewCast")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+        viewSlideShow = rootView.findViewById(R.id.home_slideshow);
+        slideshowAdapter = new SlideshowAdapter(this.getActivity());
 
-//        Toolbar toolbar = rootView.findViewById(R.id.my_toolbar);
-//        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
-//        ((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayShowTitleEnabled(false);
+        viewSlideShow.setAdapter(slideshowAdapter);
         return rootView;
     }
 
-//    @Override
-//    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-//        return super.onOptionsItemSelected(item);
-//    }
-//
-//    @Override
-//    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-//        super.onViewCreated(view, savedInstanceState);
-//        SearchView searchView = view.findViewById(R.id.search_view);
-//        searchView.setOnQueryTextFocusChangeListener(new View.OnFocusChangeListener() {
-//            @Override
-//            public void onFocusChange(View v, boolean hasFocus) {
-//                NavHostFragment.findNavController(HomeFragment.this).navigate(R.id.action_homeFragment_to_searchFragment);
-//            }
-//        });
-//        BottomNavigationView bottomNavigationView = view.findViewById(R.id.bottom_navigation);
-//        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-//            @Override
-//            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-//                switch (item.getItemId()) {
-//                    case R.id.action_home:
-//                        NavHostFragment.findNavController(HomeFragment.this).navigate(R.id.action_homeFragment_self);
-//                        break;
-//                    case R.id.action_favorites:
-//                        NavHostFragment.findNavController(HomeFragment.this).navigate(R.id.action_homeFragment_to_favoriteFragment);
-//                        break;
-//                    case R.id.action_profile:
-//                        NavHostFragment.findNavController(HomeFragment.this).navigate(R.id.action_homeFragment_to_profileFragment);
-//                        break;
-//                    case R.id.action_book:
-//                        NavHostFragment.findNavController(HomeFragment.this).navigate(R.id.action_homeFragment_to_bookingFragment);
-//                        break;
-//                }
-//                return false;
-//            }
-//        });
-//    }
 }
