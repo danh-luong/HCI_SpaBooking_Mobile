19f56e9988874ff12f2105ee702175db6eaa26ba
Cao Thu <thanhthu0321@gmail.com>
2020-02-26 20:06:56 +0700

    Change color status bar at startup activity

--- RosieBeauty/app/src/main/java/rosie/com/rosiebeauty/StartupActivity.java
+++ RosieBeauty/app/src/main/java/rosie/com/rosiebeauty/StartupActivity.java
@@ -1,8 +1,13 @@
 package rosie.com.rosiebeauty;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +16,7 @@ public class StartupActivity extends AppCompatActivity {
     private static int TIME_OUT = 3000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setStatusBarGradiant(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_startup);
         LinearLayout centerLayout = findViewById(R.id.startup_center_layout);
@@ -23,5 +29,15 @@ public class StartupActivity extends AppCompatActivity {
                 finish();
             }
         }, TIME_OUT);
+    }
+
+    protected void setStatusBarGradiant(Activity activity) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = activity.getWindow();
+            Drawable background = activity.getResources().getDrawable(R.drawable.startup_grad);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(activity.getResources().getColor(android.R.color.transparent));
+            window.setBackgroundDrawable(background);
+        }
     }
 }
