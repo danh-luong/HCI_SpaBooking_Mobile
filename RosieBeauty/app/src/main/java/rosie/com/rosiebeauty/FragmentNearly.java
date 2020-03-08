74c56e6bb4393c3ee253ea193f7ef6ef0b7077a1
Cao Thu <thanhthu0321@gmail.com>
2020-03-08 14:59:05 +0700

    Done Map

--- RosieBeauty/app/src/main/java/rosie/com/rosiebeauty/FragmentNearly.java
+++ RosieBeauty/app/src/main/java/rosie/com/rosiebeauty/FragmentNearly.java
@@ -1,31 +1,33 @@
 package rosie.com.rosiebeauty;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.drawable.Drawable;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.annotation.Nullable;
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptor;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.List;
+
 
 public class FragmentNearly extends Fragment {
+    Location currentLocation;
+    LocationManager locationManager;
+    LocationListener locationListener;
+
     public FragmentNearly() {
         // Required empty public constructor
     }
@@ -44,47 +46,85 @@ public class FragmentNearly extends Fragment {
 
         SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.mapNearly);  //use SuppoprtMapFragment for using in fragment instead of activity  MapFragment = activity   SupportMapFragment = fragment
         mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @SuppressLint("MissingPermission")
             @Override
             public void onMapReady(GoogleMap mMap) {
                 mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-
+                mMap.getUiSettings().setZoomControlsEnabled(true);
+                mMap.setMyLocationEnabled(true);
+                locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+                List<String> pr = locationManager.getProviders(true);
+                currentLocation = null;
+                for (int i = 0; i < pr.size(); i++) {
+                    currentLocation = locationManager.getLastKnownLocation(pr.get(i));
+                    if (currentLocation != null) {
+                        Log.d("bbb", currentLocation.getLatitude() + "");
+                        break;
+                    }
+                }
                 mMap.clear(); //clear old markers
 
-                CameraPosition googlePlex = CameraPosition.builder()
-                        .target(new LatLng(37.4219999,-122.0862462))
-                        .zoom(10)
-                        .bearing(0)
-                        .tilt(45)
-                        .build();
+                if (currentLocation != null) {
+                    LatLng currentPos = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+                    Log.d("aaa", "bbb");
 
-                mMap.animateCamera(CameraUpdateFactory.newCameraPosition(googlePlex), 10000, null);
-
-                mMap.addMarker(new MarkerOptions()
-                        .position(new LatLng(37.4219999, -122.0862462))
-                        .title("Spider Man"));
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentPos, 15));
+                    mMap.animateCamera(CameraUpdateFactory.zoomTo(15), 2000, null);
+                }
 
                 mMap.addMarker(new MarkerOptions()
-                        .position(new LatLng(37.4629101,-122.2449094))
-                        .title("Iron Man")
-                        .snippet("His Talent : Plenty of money"));
+                        .position(new LatLng(10.8151349, 106.697604))
+                        .title("Rose Spa")
+                        .snippet("Nơ Trang Long, Q. Bình Thạnh"));
 
                 mMap.addMarker(new MarkerOptions()
-                        .position(new LatLng(37.3092293,-122.1136845))
-                        .title("Captain America"));
+                        .position(new LatLng(10.8233244, 106.7050817))
+                        .title("Rose Spa")
+                        .snippet("Phạm Văn Đồng, Q. Bình Thạnh"));
+
+                mMap.addMarker(new MarkerOptions()
+                        .position(new LatLng(10.8148441, 106.6873383))
+                        .title("Rose Spa")
+                        .snippet("Lê Quang Định, Q. Gò Vấp"));
+
+                mMap.addMarker(new MarkerOptions()
+                        .position(new LatLng(10.8009176, 106.6817639))
+                        .title("Rose Spa")
+                        .snippet("Phan Đăng Lưu, Q. Phú Nhuận"));
+
+                mMap.addMarker(new MarkerOptions()
+                        .position(new LatLng(10.8035865, 106.6994101))
+                        .title("Rose Spa")
+                        .snippet("Phan Chu Trinh, Q. Bình Thạnh"));
+
+                mMap.addMarker(new MarkerOptions()
+                        .position(new LatLng(10.7684673, 106.6771623))
+                        .title("Rose Spa")
+                        .snippet("Nguyễn Thiện Thuật, Q.3"));
+
+                mMap.addMarker(new MarkerOptions()
+                        .position(new LatLng(10.7715641, 106.6834673))
+                        .title("Rose Spa")
+                        .snippet("Võ Văn Tần, Q.3"));
+
+                mMap.addMarker(new MarkerOptions()
+                        .position(new LatLng(10.8005214, 106.6830849))
+                        .title("Rose Spa")
+                        .snippet("Phan Xích Long, Q.Phú Nhuận"));
+
+                mMap.addMarker(new MarkerOptions()
+                        .position(new LatLng(10.7613989, 106.6900695))
+                        .title("Rose Spa")
+                        .snippet("Hồ Hảo Hớn, Q.1"));
+
+                mMap.addMarker(new MarkerOptions()
+                        .position(new LatLng(10.7894365, 106.6728024))
+                        .title("Rose Spa")
+                        .snippet("Lê Văn Sĩ, Quận 3"));
             }
         });
 
 
         return rootView;
     }
-
-//    private BitmapDescriptor bitmapDescriptorFromVector(Context context, int vectorResId) {
-//        Drawable vectorDrawable = ContextCompat.getDrawable(context, vectorResId);
-//        vectorDrawable.setBounds(0, 0, vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight());
-//        Bitmap bitmap = Bitmap.createBitmap(vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
-//        Canvas canvas = new Canvas(bitmap);
-//        vectorDrawable.draw(canvas);
-//        return BitmapDescriptorFactory.fromBitmap(bitmap);
-//    }
-
 }
