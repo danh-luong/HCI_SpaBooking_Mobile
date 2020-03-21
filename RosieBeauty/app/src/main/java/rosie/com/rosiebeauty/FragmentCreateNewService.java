ecacf0bbf67b90d1931666d6e6b452f4fe28d6f4
Cao Thu <thanhthu0321@gmail.com>
2020-03-21 16:40:11 +0700

    Manager create, not run

--- RosieBeauty/app/src/main/java/rosie/com/rosiebeauty/FragmentCreateNewService.java
+++ RosieBeauty/app/src/main/java/rosie/com/rosiebeauty/FragmentCreateNewService.java
@@ -13,12 +13,11 @@ import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 import java.util.ArrayList;
 
 import rosie.com.rosiebeauty.Adapter.ManagerViewTypeAdapter;
-import rosie.com.rosiebeauty.Adapter.MultiViewTypeAdapter;
-import rosie.com.rosiebeauty.Model.MultiViewModel;
+import rosie.com.rosiebeauty.Model.ManagerViewModel;
 
 
 public class FragmentCreateNewService extends Fragment {
-    ArrayList<MultiViewModel> gridViewModelArrayList;
+    ArrayList<ManagerViewModel> gridViewModelArrayList;
     private RecyclerView mRecyclerView;
 
     @Override
@@ -34,7 +33,7 @@ public class FragmentCreateNewService extends Fragment {
         View rootView = inflater.inflate(R.layout.item_recyler_view, container, false);
         //Recycler View
         gridViewModelArrayList = new ArrayList();
-
+        gridViewModelArrayList.add(new ManagerViewModel(ManagerViewModel.CREATE_NEW_SERVICE, "", "", "", "", "", "", "", 1));
         ManagerViewTypeAdapter adapter = new ManagerViewTypeAdapter(gridViewModelArrayList, this.getActivity().getApplicationContext());
 
         mRecyclerView = rootView.findViewById(R.id.item_recyler_view);
---
+++ RosieBeauty/app/src/main/java/rosie/com/rosiebeauty/Model/ManagerViewModel.java
@@ -0,0 +1,119 @@
+package rosie.com.rosiebeauty.Model;
+
+public class ManagerViewModel {
+
+    public static final int CREATE_NEW_SERVICE = 1;
+    public static final int SHOW_SERVICE_MANAGER = 2;
+    public int type;
+    public int data;
+    public String text;
+    public String edtNameService;
+    public String edtDesService;
+    public String edtPrice;
+    public String edtQPromotion;
+    public String edtPricePromo;
+    public String edtStartDay;
+    public String edtEndDay;
+    public int spinnerCatagory;
+
+    public ManagerViewModel(int type, String edtNameService, String edtDesService, String edtPrice,
+                            String edtQPromotion, String edtPricePromo, String edtStartDay, String edtEndDay, int spinnerCatagory) {
+        this.type = type;
+        this.edtNameService = edtNameService;
+        this.edtDesService = edtDesService;
+        this.edtPrice = edtPrice;
+        this.edtQPromotion = edtQPromotion;
+        this.edtPricePromo = edtPricePromo;
+        this.edtStartDay = edtStartDay;
+        this.edtEndDay = edtEndDay;
+        this.spinnerCatagory = spinnerCatagory;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getEdtNameService() {
+        return edtNameService;
+    }
+
+    public void setEdtNameService(String edtNameService) {
+        this.edtNameService = edtNameService;
+    }
+
+    public String getEdtDesService() {
+        return edtDesService;
+    }
+
+    public void setEdtDesService(String edtDesService) {
+        this.edtDesService = edtDesService;
+    }
+
+    public String getEdtPrice() {
+        return edtPrice;
+    }
+
+    public void setEdtPrice(String edtPrice) {
+        this.edtPrice = edtPrice;
+    }
+
+    public String getEdtQPromotion() {
+        return edtQPromotion;
+    }
+
+    public void setEdtQPromotion(String edtQPromotion) {
+        this.edtQPromotion = edtQPromotion;
+    }
+
+    public String getEdtPricePromo() {
+        return edtPricePromo;
+    }
+
+    public void setEdtPricePromo(String edtPricePromo) {
+        this.edtPricePromo = edtPricePromo;
+    }
+
+    public String getEdtStartDay() {
+        return edtStartDay;
+    }
+
+    public void setEdtStartDay(String edtStartDay) {
+        this.edtStartDay = edtStartDay;
+    }
+
+    public String getEdtEndDay() {
+        return edtEndDay;
+    }
+
+    public void setEdtEndDay(String edtEndDay) {
+        this.edtEndDay = edtEndDay;
+    }
+
+    public int getSpinnerCatagory() {
+        return spinnerCatagory;
+    }
+
+    public void setSpinnerCatagory(int spinnerCatagory) {
+        this.spinnerCatagory = spinnerCatagory;
+    }
+}
