6c68ffe3588751d68a26d8ed2019386822e01585
Danh Luong <danhltn99@gmail.com>
2020-03-22 13:07:19 +0700

    Manager Page: * Search in services

--- RosieBeauty/app/src/main/java/rosie/com/rosiebeauty/ListServiceFragment.java
+++ RosieBeauty/app/src/main/java/rosie/com/rosiebeauty/ListServiceFragment.java
@@ -4,10 +4,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class ListServiceFragment extends Fragment {
+
+    private View rootView;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,7 +23,16 @@ public class ListServiceFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list_service, container, false);
+        rootView = inflater.inflate(R.layout.fragment_list_service, container, false);
+        final EditText edtSearchService = (EditText) rootView.findViewById(R.id.edtSearchService);
+        FloatingActionButton btnSearchService = (FloatingActionButton) rootView.findViewById(R.id.btnSearchService);
+        btnSearchService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String searchService = edtSearchService.getText().toString();
+
+            }
+        });
+        return rootView;
     }
 }
